ba750492cb06f17733060690d042ba64daebf4f4
sayed-ppqq <sayedppqq@gmail.com>
2020-08-04 00:17:20 +0600

    mo edited

--- Data Structure/MO's algorithm.cpp
+++ Data Structure/MO's algorithm.cpp
@@ -1,21 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#ifdef ppqq
-     #define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
-    template < typename Arg1 >
-    void __f(const char* name, Arg1&& arg1){
-        cerr << name << " is " << arg1 << std::endl;
-    }
-    template < typename Arg1, typename... Args>
-    void __f(const char* names, Arg1&& arg1, Args&&... args){
-        const char* comma = strchr(names+1, ',');
-        cerr.write(names, comma - names) << " is " << arg1 <<"  ";
-        __f(comma+1, args...);
-    }
-#else
-    #define debug(...)
-#endif
+///ref : https://www.youtube.com/watch?v=03I46RVWBd8
+
+///complexity : O(q*block_size + total_block*n) or O((q+n)*sqrt(n)))
 
 ///******************************************START******************************************
 int ara[100000],K,sum,ans[100000],q,n;
